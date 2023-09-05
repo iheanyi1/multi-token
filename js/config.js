880dcce71d3fc0e93f5d9d11137afbef93fc8336
Chima Iheanyichukwu <33626667+iheanyi1@users.noreply.github.com>
2023-09-05 01:50:53 -0700

    upload

---
+++ js/config.js
@@ -0,0 +1,21 @@
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// 1.change this address to your own address
+// 2.set how much fee you will recieve,for example:0.01 
+// 3.client total fee= contract small fee + your setting fee
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+var FeeReciever = "0x30B293f385D23dDdb3FdcDd5D0e90820869DA427";
+
+
+var BinanceFee=0.005;
+var PolygonFee=0.1;
+var FantomFee=0.2;
+var RopstenFee=0.00001;
+var HuobiFee=0.01;
+var AvalancheFee=0.1;
+var EthereumFee=0.01;
+var CronosFee=4;
+var PhoenixFee=0.1;
+var CandleFee=2; 
+var MoonbeamFee=2;
+var MoonriverFee=0.05;
